e92133c348afc360ad513e43084fbbe055bd7809
Swapnil Shah <swapnilshah5889@gmail.com>
2024-04-10 05:57:34 -0400

    Added prisma dependency in global package @repo/db

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -3,18 +3,18 @@
 import { ReactNode } from "react";
 
 interface ButtonProps {
-  children: ReactNode;
+  children?: ReactNode;
   className?: string;
-  appName: string;
+  appName?: string;
 }
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
-      className={className}
+      className={"bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded "+`${className}`}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
-      {children}
+      Button
     </button>
   );
 };
