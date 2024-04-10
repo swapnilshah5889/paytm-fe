718f38a4c531182f796ede3d480f4fc6281bacbb
Swapnil Shah <swapnilshah5889@gmail.com>
2024-04-10 06:26:29 -0400

    Added recoil store global package

--- apps/user_app/app/page.tsx
+++ apps/user_app/app/page.tsx
@@ -1,15 +1,19 @@
+"use client"
 import Image from "next/image";
 import { Card } from "@repo/ui/card";
 import { Code } from "@repo/ui/code";
 import styles from "./page.module.css";
 import { Button } from "@repo/ui/button";
 import { PrismaClient } from "@repo/db/client";
+import { useBalance } from "@repo/store";
 
 export default function Page(): JSX.Element {
   const client = new PrismaClient();
+  const balance = useBalance();
+  console.log(balance)
   return (
     <div className="text-2xl">
-      Hi There
+      Hi There {balance}
       <Button />
     </div>
   );
